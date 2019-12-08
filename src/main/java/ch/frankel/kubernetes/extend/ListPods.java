f945fb16ccf858afcd5a58571e27f1a00d589d9e
Nicolas Frankel <nicolas@frankel.ch>
2019-12-08 15:23:00 +0200

    List pods from outside the cluster
      Relies on an existing ~/.kube/config configuration file pointing to a running Kubernetes cluster

---
+++ src/main/java/ch/frankel/kubernetes/extend/ListPods.java
@@ -0,0 +1,21 @@
+package ch.frankel.kubernetes.extend;
+
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.util.Config;
+
+public class ListPods {
+
+    public static void main(String[] args) throws Exception {
+        ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+        CoreV1Api core = new CoreV1Api();
+        V1PodList pods = core.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+        pods.getItems()
+                .stream()
+                .map(it -> "- " + it.getMetadata().getName())
+                .forEach(System.out::println);
+    }
+}
