77168bb61feaeeda557eb9472e59eb0f009a5b2b
Nicolas Frankel <nicolas@frankel.ch>
2019-12-10 13:58:08 +0100

    Migrate to Fabric8

--- src/main/java/ch/frankel/kubernetes/extend/Sidecar.java
+++ src/main/java/ch/frankel/kubernetes/extend/Sidecar.java
@@ -1,44 +1,30 @@
 package ch.frankel.kubernetes.extend;
 
-import io.kubernetes.client.informer.SharedInformerFactory;
-import io.kubernetes.client.openapi.Configuration;
-import io.kubernetes.client.openapi.apis.CoreV1Api;
-import io.kubernetes.client.openapi.models.V1Pod;
-import io.kubernetes.client.openapi.models.V1PodList;
-import io.kubernetes.client.util.Config;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodList;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Executors;
 
 public class Sidecar {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Sidecar.class);
 
-    public static void main(String[] args) throws Exception {
-        LOGGER.info("*** JVM Operator v1.7 ***");
-        var client = Config.defaultClient();
-        Configuration.setDefaultApiClient(client);
-        print();
+    public static void main(String[] args) {
+        LOGGER.info("*** JVM Operator v1.8 ***");
+        var service = Executors.newSingleThreadExecutor();
+        var client = new DefaultKubernetesClient();
+        Runtime.getRuntime().addShutdownHook(new Thread(client::close));
+        service.submit(() -> print(client));
     }
 
-    private static void print() {
-        var api = new CoreV1Api();
-        var factory = new SharedInformerFactory();
+    private static void print(KubernetesClient client) {
+        var factory = client.informers();
         var informer = factory.sharedIndexInformerFor(
-                it -> api.listPodForAllNamespacesCall(
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        it.resourceVersion,
-                        null,
-                        it.timeoutSeconds,
-                        it.watch,
-                        null),
-                V1Pod.class,
-                V1PodList.class);
-        informer.addEventHandler(new SidecarEventHandler());
-        factory.startAllRegisteredInformers();
+                Pod.class, PodList.class, 10 * 60 * 1000);
+        informer.addEventHandler(new SidecarEventHandler(client));
     }
 }
